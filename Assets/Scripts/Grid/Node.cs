d313d86c95dc4340d2cbe0b24bea2962edaad5cf
Cameron <cameron_cintron@yahoo.com>
2023-01-15 19:10:31 -0600

    - add 3d models from a free package on the asset store and configure prefabs

--- Assets/Scripts/Grid/Node.cs
+++ Assets/Scripts/Grid/Node.cs
@@ -1,4 +1,38 @@
-﻿public abstract class Node
+﻿using System.Collections.Generic;
+
+namespace CCintron.Grid
 {
-    
+    public abstract class Node
+    {
+        List<RowColumnPair> coords = new List<RowColumnPair>();
+
+        protected void Init(string pattern)
+        {
+            string[] rows = pattern.Split('\n');
+
+            for(int i = 0; i < rows.Length; i++)
+            {
+                string row = rows[i];
+
+                for(int j = 0; j < row.Length; j++)
+                {
+                    if (row[j] == 'x') coords.Add(new RowColumnPair(i, j));
+
+                }
+            }
+        }
+
+        public RowColumnPair[] GetCoordsFromPoint(int row, int column)
+        {
+            RowColumnPair[] coordsFromPoint = new RowColumnPair[coords.Count];
+
+            for(int i = 0; i < coords.Count; i++)
+            {
+                RowColumnPair coord = coords[i];
+                coordsFromPoint[i] = new RowColumnPair(coord.Row + row, coord.Column + column);
+            }
+
+            return coordsFromPoint;
+        }
+    }
 }
---
+++ Assets/Scripts/Grid/RowColumnPair.cs
@@ -0,0 +1,14 @@
+﻿namespace CCintron.Grid
+{
+    public class RowColumnPair
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public RowColumnPair(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
